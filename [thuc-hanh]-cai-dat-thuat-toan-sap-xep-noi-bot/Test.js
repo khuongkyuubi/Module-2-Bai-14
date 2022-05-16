1508636531296d64ad124675eb5e479af03bf991
My.Project <khuong.kyuubi@gmail.com>
2022-05-16 08:46:51 +0700

    thêm bài thực hành sắp xếp nổi bọt

---
+++ [thuc-hanh]-cai-dat-thuat-toan-sap-xep-noi-bot/Test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var BubleSort_1 = require("./BubleSort");
+BubleSort_1.BubbleSort.bubbleSort(BubleSort_1.BubbleSort.list);
+for (var i = 0; i < BubleSort_1.BubbleSort.list.length; i++)
+    console.log(BubleSort_1.BubbleSort.list[i] + " ");
